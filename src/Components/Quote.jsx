f440a87ccc36f3ddd5367cbd0217f87eb2b78bb2
Harunzy <Harunzy55@gmail.com>
2025-12-01 23:14:03 +0000

    add quote.jsx

---
+++ src/Components/Quote.jsx
@@ -0,0 +1,59 @@
+const quotes = [
+  {
+    id: 1,
+    text: 'Continue to grow and learn.',
+    icon: '✨',
+  },
+  {
+    id: 2,
+    text: 'Continue to grow and learn.',
+    icon: '🔑',
+  },
+  {
+    id: 2,
+    text: 'Continue to grow and learn.',
+    icon: '🚀',
+  },
+  {
+    id: 3,
+    text: 'Continue to grow and learn.',
+    icon: '🗽',
+  },
+  {
+    id: 4,
+    text: 'Continue to grow and learn.',
+    icon: '🪂',
+  },
+  {
+    id: 5,
+    text: 'Continue to grow and learn.',
+    icon: '🤸🏿',
+  },
+  {
+    id: 6,
+    text: 'Continue to grow and learn.',
+    icon: '👨‍🚀',
+  },
+  {
+    id: 7,
+    text: 'Continue to grow and learn.',
+    icon: '🎅',
+  },
+  {
+    id: 8,
+    text: 'Continue to grow and learn.',
+    icon: '⛹️‍♂️',
+  },
+  {
+    id: 9,
+    text: 'Continue to grow and learn.',
+    icon: '🚴‍♂️',
+  },
+  {
+    id: 10,
+    text: 'Continue to grow and learn.',
+    icon: '👊',
+  },
+];
+
+export default quotes;
