89e9643bc0f88a9534d058888de031b62e6ec4fb
Harunzy <Harunzy55@gmail.com>
2025-12-01 22:24:47 +0000

    add Navbar comp

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,20 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Header = () => {
+  const hours = new Date().getHours();
+  let greet;
+  if (hours < 12) {
+    greet = 'Good Morning';
+  } else if (hours >= 12 && hours < 18) {
+    greet = 'Good Afternoon';
+  } else {
+    greet = 'Good Evening';
+  }
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-        <div className="container-fluid newNav">
-          <a className="navbar-brand" href="#">
-            Taskora
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Completed
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Add
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
+      <h1>{greet}</h1>
     </div>
   );
 };
