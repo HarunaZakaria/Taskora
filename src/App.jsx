7def4ecd66a38b653f7c0d600e2e4218319d8548
Harunzy <Harunzy55@gmail.com>
2025-12-03 22:45:05 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css';
-import Header from './Components/Header';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import useLocalStorage from './hooks/useLocalStorage';
+import AddTodo from './Components/AddTodo';
+import TodoList from './Components/TodoList';
+export default function App() {
+  const [todos, setTodos] = useLocalStorage('todos', []);
 
-function App() {
+  function addTodo(text) {
+    const newTodo = {
+      id: uuidv4(),
+      text,
+      completed: false,
+      createdAt: Date.now(),
+    };
+    setTodos((prev) => [newTodo, ...prev]);
+  }
+
+  function toggleTodo(id) {
+    setTodos((prev) =>
+      prev.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos((prev) => prev.filter((t) => t.id !== id));
+  }
+
   return (
-    <>
-      <header>
-        <Header />
-      </header>
-    </>
+    <main style={{ maxWidth: 600, margin: '40px auto', padding: 16 }}>
+      <h1>To-Do App</h1>
+      <AddTodo onAdd={addTodo} />
+      <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+    </main>
   );
 }
-
-export default App;
